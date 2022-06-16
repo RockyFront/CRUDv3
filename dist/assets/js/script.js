ab95018896f2b4ecf77146d005ddd310c73f6cfe
AhmadAlArezi <42297202+AhmadAlArezi@users.noreply.github.com>
2022-06-16 17:24:35 +0430

    add fetch Data

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -1,10 +1,21 @@
 const BASE_URL = "https://62a21c77cc8c0118ef5d2e28.mockapi.io/users/";
 const LIMIT_PAGES = 5;
 
-const getData = async (url, page = 1) => {};
+const getData = async (url, page) => {
+  let mainUrl = url;
 
-const tableRender = (data = []) => {};
+  if (page) {
+    mainUrl = url + `?page=${page}&limit=${LIMIT_PAGES}`;
+  }
+  const result = await fetch(mainUrl);
+  return await result.json();
+};
 
+const tableRender = async (page = 1) => {
+  const data = await getData(BASE_URL, page);
+  console.log(data);
+};
+tableRender();
 const tablePagination = () => {};
 
 const addData = () => {};
