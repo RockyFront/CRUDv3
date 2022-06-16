7c9d2011ed2d3c3cac798851ce30a59ef86fe877
AhmadAlArezi <42297202+AhmadAlArezi@users.noreply.github.com>
2022-06-16 16:49:58 +0430

    add script structrue

---
+++ dist/assets/js/script.js
@@ -0,0 +1,16 @@
+const BASE_URL = "https://62a21c77cc8c0118ef5d2e28.mockapi.io/users/";
+const LIMIT_PAGES = 5;
+
+const getData = async (url, page = 1) => {};
+
+const tableRender = (data = []) => {};
+
+const tablePagination = () => {};
+
+const addData = () => {};
+
+const editData = (id) => {};
+
+const deleteData = (id) => {};
+
+const searchData = (id) => {};
