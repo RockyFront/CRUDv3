2b41222dec1b7a107857d4e4df829163b0eb8d76
alirezvani <rezvani1982@gmail.com>
2022-06-17 10:55:29 +0430

    modify tablerender

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -13,9 +13,53 @@ const getData = async (url, page) => {
 
 const tableRender = async (page = 1) => {
   const data = await getData(BASE_URL, page);
+  const tbody = document.getElementById("tbody");
+
   console.log(data);
+
+  data.forEach((userObj, index) => {
+    const tr = tbody.insertRow();
+    tr.setAttribute("data-id", userObj.id)
+    
+
+
+    if(Object.keys(userObj).includes("id")){
+      tr.insertCell(0).innerHTML = `${userObj.id}`;
+    }else{
+
+    }
+
+
+    Object.values(userObj).forEach((objValue) => {
+     
+      if(objValue !== userObj.id){
+
+        tr.insertCell(1).innerHTML = `${objValue}`;
+      }
+    });
+    
+    const editCell = tr.insertCell()
+    const editImg = document.createElement("img")
+    editImg.classList.add("icon");
+    editImg.src= "./assets/icons/edit.svg";
+    editCell.appendChild(editImg);
+
+    const deleteCell = tr.insertCell()
+    const deleteImg = document.createElement("img")
+    deleteImg.classList.add("icon");
+    deleteImg.src= "./assets/icons/delete.svg";
+    deleteCell.appendChild(deleteImg);
+    
+
+  });
 };
+
+
+
+
+
 tableRender();
+
 const tablePagination = () => {};
 
 const addData = () => {};
