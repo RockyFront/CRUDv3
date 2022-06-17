41ebac6e6e349f47d6a5715ab60082d58ef80b9a
Hootan Abasi Rocky <mast9rmind@gmail.com>
2022-06-17 14:09:52 +0430

    add tableRender

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -2,61 +2,94 @@ const BASE_URL = "https://62a21c77cc8c0118ef5d2e28.mockapi.io/users/";
 const LIMIT_PAGES = 5;
 
 const getData = async (url, page) => {
-  let mainUrl = url;
+	let mainUrl = url;
 
-  if (page) {
-    mainUrl = url + `?page=${page}&limit=${LIMIT_PAGES}`;
-  }
-  const result = await fetch(mainUrl);
-  return await result.json();
+	if (page) {
+		mainUrl = url + `?page=${page}&limit=${LIMIT_PAGES}`;
+	}
+	const result = await fetch(mainUrl);
+	return await result.json();
 };
 
 const tableRender = async (page = 1) => {
-  const data = await getData(BASE_URL, page);
-  const tbody = document.getElementById("tbody");
+	const data = await getData(BASE_URL, page);
+	const tbody = document.getElementById("tbody");
 
-  console.log(data);
+	console.log(data);
 
-  data.forEach((userObj, index) => {
-    const tr = tbody.insertRow();
-    tr.setAttribute("data-id", userObj.id)
-    
+	data.forEach((userObj, index) => {
+		const tr = tbody.insertRow();
+		tr.setAttribute("data-id", userObj.id);
 
+		const {
+			id,
+			name,
+			family,
+			birthday,
+			nationalID,
+			fatherName,
+			education,
+			job,
+			gender,
+			country,
+			state,
+			city,
+			street,
+			block,
+			no,
+			floor,
+			unit,
+		} = userObj;
 
-    if(Object.keys(userObj).includes("id")){
-      tr.insertCell(0).innerHTML = `${userObj.id}`;
-    }else{
+		const modifiedUserObj = [
+			id,
+			name,
+			family,
+			birthday,
+			nationalID,
+			fatherName,
+			education,
+			job,
+			gender,
+			country,
+			state,
+			city,
+			street,
+			block,
+			no,
+			floor,
+			unit,
+		];
 
-    }
+		modifiedUserObj.forEach((objValue) => {
+			const td = tr.insertCell();
+			td.innerHTML = objValue;
+		});
 
+		const editCell = tr.insertCell();
+		const editImg = document.createElement("img");
+		editImg.classList.add("icon");
+		editImg.src = "./assets/icons/edit.svg";
+		editCell.appendChild(editImg);
+		editImg.onclick = () => {
+			window.open(`./add-edit.html?status=edit&id=${id}`, "_self");
+		};
+		const deleteCell = tr.insertCell();
+		const deleteImg = document.createElement("img");
+		deleteImg.classList.add("icon");
+		deleteImg.src = "./assets/icons/delete.svg";
+		deleteCell.appendChild(deleteImg);
+		deleteImg.onclick = () => {
+			const modal = document.querySelector(".modal");
+			modal.style.display = "initial";
+			let inputs = modal.getElementsByTagName("input");
 
-    Object.values(userObj).forEach((objValue) => {
-     
-      if(objValue !== userObj.id){
-
-        tr.insertCell(1).innerHTML = `${objValue}`;
-      }
-    });
-    
-    const editCell = tr.insertCell()
-    const editImg = document.createElement("img")
-    editImg.classList.add("icon");
-    editImg.src= "./assets/icons/edit.svg";
-    editCell.appendChild(editImg);
-
-    const deleteCell = tr.insertCell()
-    const deleteImg = document.createElement("img")
-    deleteImg.classList.add("icon");
-    deleteImg.src= "./assets/icons/delete.svg";
-    deleteCell.appendChild(deleteImg);
-    
-
-  });
+			[...inputs].forEach((input, index) => {
+				input.value = modifiedUserObj[index + 1];
+			});
+		};
+	});
 };
-
-
-
-
 
 tableRender();
 
@@ -64,8 +97,12 @@ const tablePagination = () => {};
 
 const addData = () => {};
 
-const editData = (id) => {};
+function editData(id) {}
 
 const deleteData = (id) => {};
+
+function addRow() {
+	window.open(`./add-edit.html?status=add&id=${id}`, "_self");
+}
 
 const searchData = (id) => {};
