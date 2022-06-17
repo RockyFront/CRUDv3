c9b5a73cec06317965c277e4a8d56fdfe116f7d4
Mahdi-Abdi <lifevita96@gmail.com>
2022-06-17 15:03:52 +0430

    add event listner to delete button - add delete data function

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -1,5 +1,8 @@
-const BASE_URL = "https://62a21c77cc8c0118ef5d2e28.mockapi.io/users/";
+
+const BASE_URL = "https://62a21c77cc8c0118ef5d2e28.mockapi.io/users";
+
 const LIMIT_PAGES = 5;
+let idToDelete = -1;
 
 const getData = async (url, page) => {
 	let mainUrl = url;
@@ -12,8 +15,10 @@ const getData = async (url, page) => {
 };
 
 const tableRender = async (page = 1) => {
+	
 	const data = await getData(BASE_URL, page);
 	const tbody = document.getElementById("tbody");
+	tbody.innerHTML='';
 
 	console.log(data);
 
@@ -79,7 +84,9 @@ const tableRender = async (page = 1) => {
 		deleteImg.classList.add("icon");
 		deleteImg.src = "./assets/icons/delete.svg";
 		deleteCell.appendChild(deleteImg);
+		
 		deleteImg.onclick = () => {
+			idToDelete=id;
 			const modal = document.querySelector(".modal");
 			modal.style.display = "initial";
 			let inputs = modal.getElementsByTagName("input");
@@ -93,16 +100,62 @@ const tableRender = async (page = 1) => {
 
 tableRender();
 
-const tablePagination = () => {};
+const tablePagination = () => { };
 
-const addData = () => {};
+const addData = () => { };
 
-function editData(id) {}
+function editData(id) { }
 
-const deleteData = (id) => {};
+const deleteData = async (id, url) => {
+	const res = await fetch(`${url}/${id}`, {
+		method: "DELETE",
+		headers: {
+			"Content-Type": "application/json",
+		}
+
+	})
+};
 
 function addRow() {
 	window.open(`./add-edit.html?status=add&id=${id}`, "_self");
 }
 
-const searchData = (id) => {};
+const searchData = (id) => { };
+
+document.getElementById('modal-delete').addEventListener('click', async () => {
+	try {
+		await deleteData(idToDelete,BASE_URL)
+		document.querySelector('.modal').style.display='none';
+		idToDelete=-1;
+		 tableRender();
+		 Toastify({
+			text: "delete successful",
+			duration: 3000,
+			destination: "https://github.com/apvarun/toastify-js",
+			newWindow: true,
+			close: true,
+			gravity: "top", // `top` or `bottom`
+			position: "left", // `left`, `center` or `right`
+			stopOnFocus: true, // Prevents dismissing of toast on hover
+			style: {
+			  background: "linear-gradient(to right, #00b09b, #96c93d)",
+			},
+			onClick: function(){} // Callback after click
+		  }).showToast()
+	} catch (error) {
+		Toastify({
+			text: "delete failed ",
+			duration: 3000,
+			destination: "https://github.com/apvarun/toastify-js",
+			newWindow: true,
+			close: true,
+			gravity: "top", // `top` or `bottom`
+			position: "left", // `left`, `center` or `right`
+			stopOnFocus: true, // Prevents dismissing of toast on hover
+			style: {
+			  background: "linear-gradient(to right, crimson, #FF91A4)",
+			},
+			onClick: function(){} // Callback after click
+		  }).showToast()
+	}
+})
